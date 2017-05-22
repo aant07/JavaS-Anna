5f1af4f7da0db3c29b7d70bc7e9ddc63e9a1282d
aantich44 <aantich44@gmail.com>
2017-05-22 15:38:12 -0400

    Hello

---
+++ scratchpad.js
@@ -0,0 +1,20 @@
+var firstName ="Anna".toUpperCase ();
+var lastName ="Antich";
+var fullName= firstName+''+lastName;
+console.log ("Your first name is"+ firstName);
+
+console.log ("Your last name is"+lastName);
+console.log("Your full name is"+ fullName);
+console.log(fullName);
+
+var yourName = promt("What is your name?").tolowerCase();
+if (yourName.tolowerCase()===firstName.tolowerCase()) {
+  alert("Hey, I think I know you! You're"+ yourName);
+}else{
+  slert ("Oh, nice to meet you.");
+  
+  
+}
+
+
+
